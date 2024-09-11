3ccb6674628edd3336283120fbcadd728ffe0ac2
Mark McLaren <mark.mclaren@users.noreply.github.com>
2024-09-11 12:24:35 +0100

    Include package data in install

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 from setuptools import setup
+
 from alphapulldown import __version__
+
 if __name__ == '__main__':
-    setup(version=__version__)
+    setup(version=__version__, include_package_data=True)
