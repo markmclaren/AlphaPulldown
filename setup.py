971db9ae41cb83b8171ee170d797f232cd328137
Mark McLaren <mark.mclaren@users.noreply.github.com>
2024-09-11 12:32:48 +0100

    Include package_data in setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,15 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 from alphapulldown import __version__
 
-if __name__ == '__main__':
-    setup(version=__version__, include_package_data=True)
+if __name__ == "__main__":
+    setup(
+        version=__version__,
+        packages=find_packages(),
+        include_package_data=True,
+        package_data={
+            "alphapulldown": [
+                "scripts/*"
+            ],  # Include all files in the scripts directory
+        },
+    )
